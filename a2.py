bfcbc217de8457cd7ed7a22ecf4fe89ab7ea89c3
skyosito <co8mcate@gmail.com>
2025-09-26 14:45:19 -0700

    implement methods

---
+++ a2.py
@@ -0,0 +1,13 @@
+generate_public_key_vector(matrix, secret_key, error, mod)
+#return as a NumPy array
+
+
+encrypt(public_key, random, message, mod)
+#return encryption tuple
+#hint: return a tuple in Python as a set of comma-separated values
+#example - return 1,2
+#random binary vector is used to index the public key
+
+
+decrypt(private_key, cipher, mod)
+#return the message
